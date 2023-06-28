b58b67a156cabe93c705cc2e8804753f718cc3ca
Goubert Rodrigues <goubert@gmail.com>
2023-06-28 14:16:20 -0300

    inicio da página de atrações

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const documentHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--doc-height' , `${window.innerHeight}px`)
+}
+
+window.addEventListener('resize', documentHeight)
+documentHeight()
+
+
+/*
+
+const documentHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+   }
+   window.addEventListener(‘resize’, documentHeight)
+   documentHeight()
+
+   */
